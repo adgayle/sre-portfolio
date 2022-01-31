e0a51d7a6840e31dad22107a7b9737894170ee19
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-01-30 21:49:34 -0500

    Update initial test to check response message

--- apps/literature/tests/test_orders.py
+++ apps/literature/tests/test_orders.py
@@ -1,12 +1,17 @@
 """Test orders"""
 import requests
-from orders import __version__
+from orders import __version__, Publisher, Literature, db
 
 
 def test_version():
+    """Test the version"""
     assert __version__ == "0.1.0"
 
 
 def test_orders():
-    response = requests.get("http://127.0.0.1:5000")
+    """Test order response to GET"""
+    response = requests.get("http://localhost:5000/")
     assert response.status_code == 200, "Failed to get a 200 response"
+    assert (
+        response.json()["message"] == "GET response from Flask app"
+    ), "Failed to retrieve correct message"
