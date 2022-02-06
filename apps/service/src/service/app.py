25501d6630f945224257056db7bf2363e10594c3
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-02-06 17:29:51 -0500

    Add bcrypt and db initialization

--- apps/service/src/service/app.py
+++ apps/service/src/service/app.py
@@ -1,6 +1,7 @@
 """Flask application"""
 from flask import Flask
 from .config import app_config
+from .models import bcrypt, db
 
 
 def create_app(environment: str) -> Flask:
@@ -8,10 +9,7 @@ def create_app(environment: str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(app_config[environment])
 
-    @app.route("/", methods=["GET"])
-    def index():
-        """Root endpoint"""
-
-        return "Congratulations! Your first endpoint is working"
+    bcrypt.init_app(app)
+    db.init_app(app)
 
     return app
