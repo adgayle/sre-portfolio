a5bdf90f15f48df374e72ebc42b1e13b656b83d2
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-05-21 19:59:21 -0400

    Fix to only sell when we have tickets

--- booking/main.go
+++ booking/main.go
@@ -10,13 +10,13 @@ func main() {
 	const conferenceTickets = 50
 
 	var remainingTickets uint = conferenceTickets
-	var bookings []string
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	for {
+	for remainingTickets > 0 {
 		var firstName string
 		var lastName string
 		var emailAddress string
