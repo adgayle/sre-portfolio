62b9de37c2f12c2925a72ce2f77d1007494f2d0b
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-06-09 23:56:27 -0400

    Add get book by isbn

--- golang/book-library/main.go
+++ golang/book-library/main.go
@@ -24,10 +24,45 @@ func getBooks(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, books)
 }
 
+func bookByISBN(isbn string) (*book, error) {
+	for index, book := range books {
+		if book.ISBN == isbn {
+			return &books[index], nil
+		}
+	}
+
+	return nil, fmt.Errorf("book with ISBN %v not found", isbn)
+}
+
+func addBook(context *gin.Context) {
+	var newBook book
+
+	if err := context.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	context.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBookByISBN(context *gin.Context) {
+	isbn := context.Param("isbn")
+	book, err := bookByISBN(isbn)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
 	fmt.Println("Welcome to our Library")
 
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", addBook)
+	router.GET("/books/:isbn", getBookByISBN)
 	router.Run("localhost:8080")
 }
