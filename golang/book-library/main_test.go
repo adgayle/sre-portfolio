8ef7c0b5b0a9c72c8149f218d91ff103e23d8174
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-06-19 00:24:09 -0400

    Add first test

---
+++ golang/book-library/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func prettyJSON(str string) (string, error) {
+	var pretty bytes.Buffer
+
+	if err := json.Indent(&pretty, []byte(str), "", "    "); err != nil {
+		return "", err
+	}
+	return pretty.String(), nil
+}
+
+func TestHandleHomePage(t *testing.T) {
+	mockResponse := `{"message": "Welcome to our Book Library"}`
+	res, err := prettyJSON(mockResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := SetUpRouter()
+	r.GET("/", HandleHomePage)
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, res, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
+
+}
