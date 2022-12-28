5e38d8f70463254bd2ae1e58d62fc16b07eb7b82
Alquin Gayle <26052969+adgayle@users.noreply.github.com>
2022-12-27 19:15:08 -0500

    chore(book-library): add helm for deployment

--- golang/book-library/main.go
+++ golang/book-library/main.go
@@ -4,13 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // main the controlling function.
 func main() {
-	apiAddress := flag.String("address", "localhost", "Address running the API.")
+	apiAddress := flag.String("address", "", "Address running the API.")
 	apiPort := flag.String("port", "8080", "Book API HTTP port.")
 	releaseMode := flag.Bool("release-mode", true, "Gin execution mode true=release|false=debug.")
 
@@ -30,10 +31,22 @@ func main() {
 	router.PATCH("/checkout", CheckoutBook)
 	router.PATCH("/return", ReturnBook)
 	router.DELETE("/books/:isbn", DeleteBook)
+	router.GET("/healthz", healthCheck)
+	router.GET("/readyz", readyCheck)
 
 	addr := fmt.Sprintf("%v:%v", *apiAddress, *apiPort)
 	err = router.Run(addr)
 	if err != nil {
 		log.Fatalf("Failed to run router on %v with error %v", addr, err)
 	}
+
+	// TODO: add a proper shutdown.
+}
+
+func healthCheck(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Status OK"})
+}
+
+func readyCheck(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Status OK"})
 }
